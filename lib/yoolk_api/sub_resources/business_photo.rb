a7e2717b4d76fc16fb6062dc1dfed86c655e89ce
Vuthy San <saravuthy@gmail.com>
2013-12-18 10:19:20 +0700

    [#62607738] remove unused code

--- lib/yoolk_api/sub_resources/business_photo.rb
+++ lib/yoolk_api/sub_resources/business_photo.rb
@@ -1,10 +1,5 @@
 module YoolkApi
   class BusinessPhoto < SubResource
-    attr_reader :business_photos
 
-    def initialize(attributes={}, business_photos=[])
-      super(attributes)
-      @business_photos = business_photos
-    end
   end
 end
